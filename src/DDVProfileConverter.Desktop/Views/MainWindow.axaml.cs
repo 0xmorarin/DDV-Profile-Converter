e7edab8ea6aebd441ce970d36f96945b4d8f75c6
morarin <130b84a@gmail.com>
2026-09-11 14:19:13 +0900

    Initial project structure

---
+++ src/DDVProfileConverter.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DDVProfileConverter.Desktop.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
