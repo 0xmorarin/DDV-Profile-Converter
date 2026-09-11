dc72d715e8a3540e36251542ff60a99f88293c6e
morarin <130b84a@gmail.com>
2026-09-11 14:48:15 +0900

    Create ProfileJsonTests.cs

---
+++ tests/DDVProfileConverter.Tests/ProfileJsonTests.cs
@@ -0,0 +1,89 @@
+using System.Text;
+using DDVProfileConverter.Core.Profile;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDVProfileConverter.Tests;
+
+[TestClass]
+public sealed class ProfileJsonTests
+{
+    [TestMethod]
+    public void ReadMetadataExtractsExpectedValues()
+    {
+        var metadata = ProfileJson.ReadMetadata(
+            TestProfileFactory.CreateJson());
+
+        Assert.AreEqual(608, metadata.Version);
+        Assert.AreEqual(
+            "0123456789ABCDEF",
+            metadata.LastCustomIdOwner);
+        Assert.AreEqual(
+            "2026-09-11T00:00:00.123456700Z",
+            metadata.Modified);
+        Assert.AreEqual(
+            "TestPlayer",
+            metadata.PlayerName);
+    }
+
+    [TestMethod]
+    public void ReadMetadataRejectsMissingGameInfo()
+    {
+        var json = Encoding.UTF8.GetBytes(
+            "{\"Player\":{\"Name\":\"TestPlayer\"}}");
+
+        Assert.ThrowsExactly<InvalidDataException>(
+            () => ProfileJson.ReadMetadata(json));
+    }
+
+    [TestMethod]
+    public void ReadMetadataRejectsInvalidVersion()
+    {
+        var json = Encoding.UTF8.GetBytes(
+            "{\"GameInfo\":{\"Version\":\"608\"}}");
+
+        Assert.ThrowsExactly<InvalidDataException>(
+            () => ProfileJson.ReadMetadata(json));
+    }
+
+    [TestMethod]
+    public void TryReadMetadataRejectsArbitraryJson()
+    {
+        var json = Encoding.UTF8.GetBytes(
+            "{\"Value\":123}");
+
+        var result = ProfileJson.TryReadMetadata(
+            json,
+            out var metadata);
+
+        Assert.IsFalse(result);
+        Assert.IsNull(metadata);
+    }
+
+    [TestMethod]
+    public void PrettyPrintUsesFourSpaceIndentation()
+    {
+        var prettyBytes = ProfileJson.PrettyPrint(
+            TestProfileFactory.CreateJson());
+
+        var pretty = Encoding.UTF8.GetString(prettyBytes);
+
+        StringAssert.Contains(
+            pretty,
+            "\n    \"GameInfo\": {");
+
+        Assert.IsFalse(pretty.Contains('\r'));
+    }
+
+    [TestMethod]
+    public void MinifyRestoresCompactJson()
+    {
+        var pretty = ProfileJson.PrettyPrint(
+            TestProfileFactory.CreateJson());
+
+        var minified = ProfileJson.Minify(pretty);
+
+        Assert.AreEqual(
+            TestProfileFactory.JsonText,
+            Encoding.UTF8.GetString(minified));
+    }
+}
