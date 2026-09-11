f759fffc84b91bc17f1b3c522e24b8b98b4ceb04
morarin <130b84a@gmail.com>
2026-09-11 14:48:51 +0900

    Create ProfileCryptoTests.cs

---
+++ tests/DDVProfileConverter.Tests/ProfileCryptoTests.cs
@@ -0,0 +1,43 @@
+using System.Security.Cryptography;
+using DDVProfileConverter.Core.Crypto;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDVProfileConverter.Tests;
+
+[TestClass]
+public sealed class ProfileCryptoTests
+{
+    [TestMethod]
+    public void DecryptRestoresOriginalArchive()
+    {
+        var archive = TestProfileFactory.CreateArchive(
+            ("profile", TestProfileFactory.CreateJson()));
+
+        var encrypted = TestProfileFactory.Encrypt(archive);
+
+        var decrypted = ProfileCrypto.Decrypt(encrypted);
+
+        CollectionAssert.AreEqual(archive, decrypted);
+    }
+
+    [TestMethod]
+    public void DecryptRejectsInvalidBlockLength()
+    {
+        var input = new byte[]
+        {
+            1,
+            2,
+            3
+        };
+
+        Assert.ThrowsExactly<CryptographicException>(
+            () => ProfileCrypto.Decrypt(input));
+    }
+
+    [TestMethod]
+    public void DecryptRejectsEmptyInput()
+    {
+        Assert.ThrowsExactly<CryptographicException>(
+            () => ProfileCrypto.Decrypt(Array.Empty<byte>()));
+    }
+}
