acebd2a4f332a030da98c43bce100afdaedd6446
morarin <130b84a@gmail.com>
2026-09-11 15:10:28 +0900

    Create TemporaryDirectory.cs

---
+++ tests/DDVProfileConverter.Tests/TemporaryDirectory.cs
@@ -0,0 +1,25 @@
+namespace DDVProfileConverter.Tests;
+
+internal sealed class TemporaryDirectory : IDisposable
+{
+    public TemporaryDirectory()
+    {
+        Path = System.IO.Path.Combine(
+            System.IO.Path.GetTempPath(),
+            $"DDVProfileConverter.Tests.{Guid.NewGuid():N}");
+
+        Directory.CreateDirectory(Path);
+    }
+
+    public string Path { get; }
+
+    public void Dispose()
+    {
+        if (Directory.Exists(Path))
+        {
+            Directory.Delete(
+                Path,
+                recursive: true);
+        }
+    }
+}
