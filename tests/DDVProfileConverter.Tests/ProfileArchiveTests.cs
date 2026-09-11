e101b05e4f3d724390ef01d7599cd914ac810ad9
morarin <130b84a@gmail.com>
2026-09-11 14:48:32 +0900

    Create ProfileArchiveTests.cs

---
+++ tests/DDVProfileConverter.Tests/ProfileArchiveTests.cs
@@ -0,0 +1,45 @@
+using System.Text;
+using DDVProfileConverter.Core.Archive;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDVProfileConverter.Tests;
+
+[TestClass]
+public sealed class ProfileArchiveTests
+{
+    [TestMethod]
+    public void ExtractProfileReturnsExpectedBytes()
+    {
+        var expected = TestProfileFactory.CreateJson();
+
+        var archive = TestProfileFactory.CreateArchive(
+            ("profile", expected));
+
+        var actual = ProfileArchive.ExtractProfile(archive);
+
+        CollectionAssert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void ExtractProfileRejectsMissingProfileEntry()
+    {
+        var archive = TestProfileFactory.CreateArchive(
+            ("other", Encoding.UTF8.GetBytes("data")));
+
+        Assert.ThrowsExactly<InvalidDataException>(
+            () => ProfileArchive.ExtractProfile(archive));
+    }
+
+    [TestMethod]
+    public void ExtractProfileRejectsDuplicateProfileEntries()
+    {
+        var json = TestProfileFactory.CreateJson();
+
+        var archive = TestProfileFactory.CreateArchive(
+            ("profile", json),
+            ("profile", json));
+
+        Assert.ThrowsExactly<InvalidDataException>(
+            () => ProfileArchive.ExtractProfile(archive));
+    }
+}
